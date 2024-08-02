a37e96c0054192c2f6b8af71ebbf2fbaadda9c4f
Calvin D <calvindonaldson1997313@gmail.com>
2024-08-02 16:47:55 +0100

    mobile nav

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faFacebookSquare, faInstagram, faTiktok, faXTwitter } from '@fortawesome/free-brands-svg-icons'
 import Logo from "../../Images/optimiserables_Logo.png"
+import { useState } from "react"
 import "./Navigation.css"
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navigation() {
+
+  const [toggle, setToggle] = useState(false);
+
+  const toggleNav = () => {
+    setToggle(!toggle);
+}
+
+
+
   return (
     <nav>
+      <div id="navbar" onClick={toggleNav} className={`navSidebar ${toggle ? "showNav" : ""}`}>
+      <button onClick={toggleNav} className="navbarToggler">
+                <FontAwesomeIcon icon={toggle ? faTimes : faBars} />
+        </button>
+            <div className="navInner">
+              <img src={Logo} alt="Optimiserables Logo" className='OpLogo'/>
+                <ul className="navMenu">
+                  
+
+                </ul>
+            </div>
+        </div>
+       
       <div className='logoContainer'>
         <img src={Logo} alt="Optimiserables Logo" className='OpLogo'/>
       </div>
