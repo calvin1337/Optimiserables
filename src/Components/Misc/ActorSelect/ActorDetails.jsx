5ccdab2fde47d6996d6cbb11e929a37c99e41f3e
DavyMcL <davy93mcleish@gmail.com>
2024-08-28 17:05:55 +0100

    Added wardrobe feature. Can toggle the wardrobe and select an image to update the main Image.

--- src/Components/Misc/ActorSelect/ActorDetails.jsx
+++ src/Components/Misc/ActorSelect/ActorDetails.jsx
@@ -3,28 +3,41 @@ import { useEffect } from "react";
 
 function ActorDetails({ actor }) {
   const [currentImage, setCurrentImage] = useState(actor.src);
+  const [currentStyle, setCurrentStyle] = useState("");
+  const [isWardrobeOpen, setIsWardrobeOpen] = useState(false);
 
   useEffect(() => {
     setCurrentImage(actor.src);
+    setCurrentStyle("");
   }, [actor]);
 
-  const wardrobeClick = (src) => {
+  const wardrobeClick = (src, style) => {
+    console.log("Wardrobe clik", src, "style", style);
     setCurrentImage(src);
+    setCurrentStyle(style || "");
+  };
+
+  const toggleWardrobe = () => {
+    setIsWardrobeOpen(!isWardrobeOpen);
   };
 
   return (
-    // flex flex-col md:flex-row items-start justify-center space-y-4 md:space-y-0 md:space-x-8 mb-8
     <div className="flex flex-row justify-center  space-y-0 space-x-8">
-      
       <div className="">
         <img
-          className="w-64 h-64 object-cover"
+          className={`w-64 h-64 object-cover ${currentStyle}`}
           src={currentImage}
           alt={actor.name}
         />
-        <p>Wardrobe</p>
+        <p className="cursor-pointer" onClick={toggleWardrobe}>
+          Wardrobe{" "}
+          <span className="text-blue-500">
+            {isWardrobeOpen ? "close" : "open"}
+          </span>
+        </p>
+
         {/* Social Links */}
-                {actor.socials && actor.socials.length > 0 && (
+        {actor.socials && actor.socials.length > 0 && (
           <div className="bg-[#6A3276]/50">
             <ul className="flex space-x-4 justify-evenly">
               {actor.socials.map((social, index) => (
@@ -47,25 +60,35 @@ function ActorDetails({ actor }) {
       <div className="w-2/5 text-left">
         <h2 className="text-3xl font-semibold mb-2">{actor.name}</h2>
         <p className="text-gray-600 mb-4">{actor.bio || "No bio available."}</p>
-        
-        {/*wardrobe*/}
-        {/* <div className="mt-4">
-          <h3 className="text-xl font-semibold mb-2">Wardrobe</h3>
-          <div className="flex space-x-4">
-            {actor.wardrobe.map((outfit, index) => (
-              <img
-                key={index}
-                src={outfit.src}
-                alt={`Wardrobe ${index + 1}`}
-                className={`w-24 h-24 object-cover border-2 ${outfit.className} cursor-pointer`}
-                onClick={() => wardrobeClick(outfit.src)}
-              />
-            ))}
-          </div>
-        </div> */}
-      </div>
 
+        {/*wardrobe
+        Selecting an image in the wardrobe updates the main image
+        However, When selecting another image that isnt the orginal image puts it at the top of the layer -
+        so that it covers the logo.
+        Currently cant figure out why - will try figure it out later
+        - Davy
+        */}
+        
+        {isWardrobeOpen && actor.wardrobe && actor.wardrobe.length > 0 ? (
+          <div className="mt-4">
+            <h3 className="text-xl font-semibold mb-2">Wardrobe</h3>
+            <div className="flex space-x-4">
+              {actor.wardrobe.map((outfit, index) => (
+                <img
+                  key={index}
+                  src={outfit.src}
+                  alt={`Wardrobe ${index + 1}`}
+                  className={`w-24 h-24 object-cover border-2 ${outfit.className} cursor-pointer`}
+                  onClick={() => wardrobeClick(outfit.src, outfit.className)}
+                />
+              ))}
+            </div>
+          </div>
+        ) : (
+          isWardrobeOpen && <p>No wardrobe options available.</p>
+        )}
       </div>
+    </div>
   );
 }
 
--- src/Components/Misc/ActorSelect/ActorSelect.jsx
+++ src/Components/Misc/ActorSelect/ActorSelect.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import ActorSingle from './ActorSingle'
-import "./ActorStyles.css"
-import { useState } from 'react';
-import RandomButton from './RandomButton';
-import ActorDetail from './ActorDetails';
+import React from "react";
+import ActorSingle from "./ActorSingle";
+import "./ActorStyles.css";
+import { useState } from "react";
+import RandomButton from "./RandomButton";
+import ActorDetail from "./ActorDetails";
 
 function ActorSelect() {
-
   const [activeActor, setActiveActor] = useState(0);
   const [selectionCount, setSelectionCount] = useState(0);
-
 
   const handleActorClick = (index) => {
     setActiveActor(index);
@@ -33,78 +31,95 @@ function ActorSelect() {
 
     selectRandomActor(selectionCount);
   };
-  
 
   // Mock data for just now
   const actors = [
-    { name: 'Michael Garden', 
-      src: 'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg',
-      bio:'Michael is an upcoming actor and writer charging in to perform at the Fringe for the 1st time in 2025. He will also be our costume designer which means he’ll be hunting through charity shops and hoping for the best.', 
+    {
+      name: "Michael Garden",
+      src: "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg",
+      bio: "Michael is an upcoming actor and writer charging in to perform at the Fringe for the 1st time in 2025. He will also be our costume designer which means he’ll be hunting through charity shops and hoping for the best.",
       socials: [
-        {platform: 'Instagram', link: 'https://www.instagram.com/michaeljg212/'},
-        {platform: 'Spotlight', link: 'https://app.spotlight.com/2210-7837-6789'},
-        {platform: 'X', link: 'https://x.com/MichaelJGarden1'}
+        {
+          platform: "Instagram",
+          link: "https://www.instagram.com/michaeljg212/",
+        },
+        {
+          platform: "Spotlight",
+          link: "https://app.spotlight.com/2210-7837-6789",
+        },
+        { platform: "X", link: "https://x.com/MichaelJGarden1" },
       ],
       wardrobe: [
-        {src:'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg', style: 'Original'},
-        {src: 'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg', style:'Hue Rotate 90', className: 'hue-rotate-90'}
-      ]
+        {
+          src: "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg",
+          style: "Original",
+        },
+        {
+          src: "https://cdn.britannica.com/70/234870-050-D4D024BB/Orange-colored-cat-yawns-displaying-teeth.jpg",
+          style: "Hue Rotate 90",
+          className: "hue-rotate-90",
+        },
+      ],
     },
 
-    { name: 'Ryan', 
-      src: 'https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_3x4.jpg',
-    
+    {
+      name: "Ryan",
+      src: "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_3x4.jpg",
     },
 
-    { name: 'Finlay Donald', 
-      src: 'https://cdn.britannica.com/70/234870-050-D4D024BB/Orange-colored-cat-yawns-displaying-teeth.jpg',
-      bio: 'Finlay is a actor who has a flair for shakespeare but also enjoy’s the simple things in life such as collaboration and creativity to bring amazing theater to the public. See him and The Optimiserables at Fringe 2025',
+    {
+      name: "Finlay Donald",
+      src: "https://cdn.britannica.com/70/234870-050-D4D024BB/Orange-colored-cat-yawns-displaying-teeth.jpg",
+      bio: "Finlay is a actor who has a flair for shakespeare but also enjoy’s the simple things in life such as collaboration and creativity to bring amazing theater to the public. See him and The Optimiserables at Fringe 2025",
       socials: [
-        {platform: 'Instagram', link: 'https://www.instagram.com/finlaywilldonald/?next=%2F'}
-      ]
+        {
+          platform: "Instagram",
+          link: "https://www.instagram.com/finlaywilldonald/?next=%2F",
+        },
+      ],
     },
 
-    { name: 'Davy', 
-      src: 'https://media.istockphoto.com/id/1361394182/photo/funny-british-shorthair-cat-portrait-looking-shocked-or-surprised.jpg?s=612x612&w=0&k=20&c=6yvVxdufrNvkmc50nCLCd8OFGhoJd6vPTNotl90L-vo=',
-      bio: 'Meow' 
-    },
-      
-    { name: 'Calvin', 
-      src: 'https://static.vecteezy.com/system/resources/thumbnails/007/234/162/small_2x/bengal-cat-resting-on-the-couch-funny-with-glasses-photo.jpg' 
+    {
+      name: "Davy",
+      src: "https://media.istockphoto.com/id/1361394182/photo/funny-british-shorthair-cat-portrait-looking-shocked-or-surprised.jpg?s=612x612&w=0&k=20&c=6yvVxdufrNvkmc50nCLCd8OFGhoJd6vPTNotl90L-vo=",
+      bio: "Meow",
     },
 
-    { name: 'Paing', 
-      src: 'https://catamazing.com/cdn/shop/files/catshark.jpg?v=1649869148' 
+    {
+      name: "Calvin",
+      src: "https://static.vecteezy.com/system/resources/thumbnails/007/234/162/small_2x/bengal-cat-resting-on-the-couch-funny-with-glasses-photo.jpg",
     },
 
-    { name: 'Brooke Smith', 
-      src: 'https://static.scientificamerican.com/sciam/cache/file/2AE14CDD-1265-470C-9B15F49024186C10_source.jpg?w=1200', 
-      bio: 'Brook is an upcoming actor who is having his fist performance at the Fringe in 2025, he’s the groups budgeting manager and will be making sure not too much ‘££’ get’s spent. Come see him and the fantastic Optimiserables at the Fringe 2025!'
+    {
+      name: "Paing",
+      src: "https://catamazing.com/cdn/shop/files/catshark.jpg?v=1649869148",
     },
 
+    {
+      name: "Brooke Smith",
+      src: "https://static.scientificamerican.com/sciam/cache/file/2AE14CDD-1265-470C-9B15F49024186C10_source.jpg?w=1200",
+      bio: "Brook is an upcoming actor who is having his fist performance at the Fringe in 2025, he’s the groups budgeting manager and will be making sure not too much ‘££’ get’s spent. Come see him and the fantastic Optimiserables at the Fringe 2025!",
+    },
   ];
 
   return (
-
     <div className="flex flex-col mt-6">
-    
-    <ActorDetail actor={actors[activeActor]} />
-    <div className='mx-auto my-4 mb-12 p-1.5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 '>
-    
-    {actors.map((actor, index) => (
-        <ActorSingle
-          key={index}
-          index={index}
-          name={actor.name}
-          src={actor.src}
-          isActive={index === activeActor}
-          onClick={() => handleActorClick(index)}
-        />
-      ))}      
-     <RandomButton onClick={() => randomSelect()} />
+      <ActorDetail actor={actors[activeActor]} />
+      <div className="mx-auto my-4 mb-12 p-1.5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 ">
+        {actors.map((actor, index) => (
+          <ActorSingle
+            key={index}
+            index={index}
+            name={actor.name}
+            src={actor.src}
+            isActive={index === activeActor}
+            onClick={() => handleActorClick(index)}
+          />
+        ))}
+        <RandomButton onClick={() => randomSelect()} />
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default ActorSelect
+export default ActorSelect;
