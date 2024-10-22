c72986863c797575285f3ac5aaf05a14463eb068
Calvin D <calvindonaldson1997313@gmail.com>
2024-10-22 14:20:04 +0100

    updates

--- src/Containers/Upcoming.js
+++ src/Containers/Upcoming.js
@@ -6,6 +6,14 @@ export default function Upcoming() {
       <h1 className='lg:text-8xl'>
         Upcoming Shows
       </h1>
+      <div className="flex w-full">
+      <div className="flex-1 bg-blue-500 p-4">
+        Div 1
+      </div>
+      <div className="flex-1 bg-green-500 p-4">
+        Div 2
+      </div>
+    </div>
       </div>
   )
 }
