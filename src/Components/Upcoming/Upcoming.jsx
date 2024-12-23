29d1d40d635a6fdca861b9058afe6a7c23fe1d66
Calvin D <calvindonaldson1997313@gmail.com>
2024-12-23 12:50:52 +0000

    coming soon v3

--- src/Components/Upcoming/Upcoming.jsx
+++ src/Components/Upcoming/Upcoming.jsx
@@ -4,58 +4,54 @@ import ShowDetails from './ShowDetails';
 import {faFacebookSquare, faInstagram, faTiktok, faXTwitter } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const imagePath = require(`../../Images/baldo_bros.png`);
+const imagePath = require(`../../Images/Members/optimiserablesframe1.png`);
 
 const Upcoming = () => (
   <div className="flex flex-col items-center w-screen h-screen"> 
-    <div className="flex flex-col w-full md:w-5/6 h-full">
+    <div className="flex flex-col w-full md:w-5/6 h-full pt-5 pb-5">
       <UpcomingTitle />
-      <div className='flex flex-col lg:flex-row w-full h-4/5 p-4 gap-2'>
+      <div className='flex flex-col lg:flex-row w-full h-4/5 md:p-4 gap-5'>
         
         {/* Image Container */}
-        <div className='w-full lg:w-1/2 h-full flex flex-col gap-2'>
-        <div className="w-full flex flex-col md:flex-col justify-center items-center h-2/5 md:h-4/5 relative">
-          <div className="relative w-4/5 h-full border-4 border-[#6A3276]">
+        <div className="w-full md:w-1/2 flex flex-col md:flex-col justify-center items-center h-2/5 md:h-4/5 relative md:p-10">
+          <div className="relative w-4/5 h-full">
             <div className="backlight absolute top-0 left-0 right-0 bottom-0 z-[-1]"></div>
             <img
               src={imagePath}
               alt="Op Cover"
               className="hover-image object-fill w-full h-full mx-auto"
             />
-            </div>
           </div>
-           <div className='w-full'>
+           <div className='w-full h-1/5'>
            <ul id="countdown" className="relative w-full mx-auto p-4 text-white border-y-2 border-[#6A3276] font-narrow font-bold flex justify-around">
               <li className="flex flex-col items-center">
-                <div className="text-[6vw] md:text-[4vw] lg:text-[3vw]">00</div>
+                <div className="text-[3vw] md:text-[4vw] lg:text-[3vw]">00</div>
                 <div className="label text-[#E4CC5D] text-[2vw] md:text-[1.5vw] uppercase">Days</div>
               </li>
               <li className="flex flex-col items-center">
-                <div className="text-[6vw] md:text-[4vw] lg:text-[3vw]">00</div>
+                <div className="text-[3vw] md:text-[4vw] lg:text-[3vw]">00</div>
                 <div className="label text-[#E4CC5D] text-[2vw] md:text-[1.5vw] uppercase">Hours</div>
               </li>
               <li className="flex flex-col items-center">
-                <div className="text-[6vw] md:text-[4vw] lg:text-[3vw]">00</div>
+                <div className="text-[3vw] md:text-[4vw] lg:text-[3vw]">00</div>
                 <div className="label text-[#E4CC5D] text-[2vw] md:text-[1.5vw] uppercase">Minutes</div>
               </li>
               <li className="flex flex-col items-center">
-                <div className="text-[6vw] md:text-[4vw] lg:text-[3vw]">00</div>
+                <div className="text-[3vw] md:text-[4vw] lg:text-[3vw]">00</div>
                 <div className="label text-[#E4CC5D] text-[2vw] md:text-[1.5vw] uppercase">Seconds</div>
               </li>
             </ul>
            </div>
         </div>
          {/* Content Container */}
-         <div className='w-full lg:w-1/2 h-full flex flex-col gap-4 justify-center items-center'>
+         <div className='w-full lg:w-1/2 h-2/5 md:h-4/5 flex flex-col gap-4 justify-center items-center'>
             <h2 className="text-xl sm:text-2xl lg:text-4xl">Edinburgh Fringe 2025</h2>
             <p className="text-xs sm:text-lg md:text-xl lg:text-2xl text-white w-full">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur at porro, deserunt cupiditate provident voluptas officiis minima distinctio id cumque magnam quidem vero excepturi sapiente tempore accusamus sed, deleniti, fuga itaque. Omnis et tempora tempore sint, officia veritatis repellat! Exercitationem.
             </p>
-            <div className='w-1/2 items-center'>
-              <button className="button">
-                Get Notified
-              </button>
-            </div>
+            <a href="https://example.com/buy-tickets" target="_blank" rel="noopener noreferrer" className="text-white py-1 px-2 md:py-2 md:px-4 rounded hover:opacity-80" style={{ backgroundColor: "#6A3276" }}>
+              Get Notified
+            </a>
         </div>
        {/* Video Container */}
         <div className='w-full lg:w-1/3 h-full flex-col items-center justify-center gap-2 hidden'>
--- src/Components/Upcoming/UpcomingTitle.jsx
+++ src/Components/Upcoming/UpcomingTitle.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const UpcomingTitle = () => (
-  <div className="w-full flex justify-center items-center" style={{ height: "10%" }}>
+  <div className="w-full flex justify-center items-center " style={{ height: "10%" }}>
       <h1 className="text-2xl sm:text-3xl xl:text-5xl">Coming Soon</h1>
   </div>
 );
